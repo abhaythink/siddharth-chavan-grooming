299b2af17fa4a09dd219f7e60228308c57be3d52
Siddharth <siddharth.chavan@thinkitive.com>
2025-06-03 19:04:43 +0530

    Added React Practice Files for Day1

---
+++ ReactJS/Day1/first_react_app/src/App.jsx
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpe/NewExpense";
+
+function App() {
+	return (
+		<>
+			<NewExpense />
+			<Expenses />
+		</>
+	);
+}
+export default App;
